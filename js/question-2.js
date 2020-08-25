d3dbe63fe1a5e0aa6e84be0b813cb2d9070cf5c0
Suphachai Hestmark <top_hestmark@yahoo.no>
2020-08-25 22:29:12 +0200

    Addedd error handling

--- js/question-2.js
+++ js/question-2.js
@@ -3,26 +3,33 @@ const resultsContainer = document.querySelector(".results");
 
 async function getGames() {
 
-  const response = await fetch(url);
-  const results  = await response.json();
-  const facts = results.results;
+  try {
+    const response = await fetch(url);
+    const results  = await response.json();
+    // console.log(results)
+    const facts = results.results;
 
-  resultsContainer.innerHTML = "";
+    resultsContainer.innerHTML = "";
 
-  for (let i = 0; i < facts.length; i++) {
-    if (i === 8) {
-      break;
+    for (let i = 0; i < facts.length; i++) {
+      if (i === 8) {
+        break;
+      }
+
+      const numberOfTags = facts[i].tags.length;
+
+      resultsContainer.innerHTML += `<div class="result">
+        <p>Name: <b>${facts[i].name}</b></p>
+        <p>Rating: <b>${facts[i].rating}</b></p>
+        <p>Number of Tags: <b>${numberOfTags}</b></p>
+        </div>`
     }
-
-    const numberOfTags = facts[i].tags.length;
-
-    resultsContainer.innerHTML += `<div class="result">
-      <p>Name: <b>${facts[i].name}</b></p>
-      <p>Rating: <b>${facts[i].rating}</b></p>
-      <p>Number of Tags: <b>${numberOfTags}</b></p>
-      </div>`
   }
-
+  catch(error) {
+    // console.log("An error occured");
+    resultsContainer.innerHTML =  displayError("An error occured when calling for API");
+  }
+  
 }
 
 getGames()
